b049624ad30735c191c3fd78400e003b26e0011b
Yepu Hou <yhou0023@student.monash.edu>
2023-04-19 16:45:33 +1000

    some css styling needs to be done

--- my-app/src/components/Consumer.js
+++ my-app/src/components/Consumer.js
@@ -3,7 +3,7 @@ import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
+import '../css/Consumer.css';
 function Consumer() {
   return (
     <Container>
